0eb3094ea81116b95543a9729894e7c7185837bb
EspeIgira <Mapapuro2>
2019-02-11 16:57:21 +0200

    credential class created

---
+++ credential.py
@@ -0,0 +1,14 @@
+class Credential:
+    '''
+    Class that generates new instances of Credential
+    '''
+    credential_list = []
+
+    def __init__(self,user_account,Username,password,email):
+        
+        # docstring to document
+
+        self.user_account = user_account
+        self.Username = Username
+        self.password = password
+        self.email = email
