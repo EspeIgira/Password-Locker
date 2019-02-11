3a4c8a2f730eddc0b6bb44d45c3d34aae32ccedf
EspeIgira <Mapapuro2>
2019-02-11 14:31:17 +0200

    User test info added

--- user.py
+++ user.py
@@ -3,3 +3,12 @@ class User:
     Class that generates new instances of contacts
     """
     user_list = [] # Empty user list
+
+    def __init__(self,user_account,Username,password,email):
+
+      # docstring to document
+
+        self.user_account = user_account
+        self.Username = Username
+        self.password = password
+        self.email = email
---
+++ user_test.py
@@ -0,0 +1,15 @@
+import unittest # Importing the unittest module
+from user import user # Importing the contact class    
+
+
+def __init__(self,user_account,Username,password,email):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            user_account: New user user account.
+            Username : New user Username.
+            password: New user password.
+            email : New user email address.
+        '''
