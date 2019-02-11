6394417cb065ac308ed332d89b4c418900f22b6e
EspeIgira <Mapapuro2>
2019-02-11 17:13:10 +0200

    Credential info created

--- credential_test.py
+++ credential_test.py
@@ -1,2 +1,13 @@
 import unittest
 from credential import Credential
+
+class TestCredential(unittest.TestCase):
+
+    def setUp(self):
+
+        self.new_credential = Credential("EspeIgira","******") # create Credential object
+    
+    def test_init(self):
+
+        self.assertEqual(self.new_user.Username,"EspeIgira")
+        self.assertEqual(self.new_user.password,"******")
